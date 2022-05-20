489dd1ced07ae4a863a3a9e6b3a1ec50800dd6bc
Maxim Stropsa <mstropsa@complexica.com>
2022-05-20 19:07:54 +0300

    Utils

---
+++ src/main/java/services/RootService.java
@@ -0,0 +1,11 @@
+package services;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RootService {
+
+}
--- src/main/java/utils/DigitsCalculation.java
+++ src/main/java/utils/DigitsCalculation.java
@@ -3,4 +3,8 @@ package utils;
 
 public class DigitsCalculation {
 
+    public Double calculateRoot(Double digit){
+        return Math.sqrt(digit);
+    }
+
 }
