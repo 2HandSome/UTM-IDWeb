0d87572e31a02abc61c116692ccbdb71487f9735
Maxim Stropsa <mstropsa@complexica.com>
2022-05-20 18:07:21 +0300

    Launch Project

---
+++ src/test/java/com/example/utmidweb/UtmIdWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.utmidweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtmIdWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
