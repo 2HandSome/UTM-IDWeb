d70649c7228055e6f10c17dd9b256b6f02fff341
Maxim Stropsa <mstropsa@complexica.com>
2022-05-21 13:08:58 +0300

    Done

--- src/main/java/com/example/controller/DigitController.java
+++ src/main/java/com/example/controller/DigitController.java
@@ -3,7 +3,6 @@ package com.example.controller;
 
 import com.example.dto.DigitsDto;
 import com.example.services.RootService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
