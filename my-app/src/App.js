135f5808c68f85d8e55a325727ac7faafefcf122
alexandr <alexandrlupu34@gmail.com>
2022-05-21 14:50:27 +0300

    Add form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
-
+import WrappedAdvancedSearchForm from "./WrappedAdvancedSearchForm";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    <WrappedAdvancedSearchForm />
-    <div className="search-result-list">Search Result List</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <WrappedAdvancedSearchForm/>
+            <div className="search-result-list">Search Result List</div>
+        </div>
+    );
 }
 
 export default App;
+
